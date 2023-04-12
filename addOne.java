842200939e20a977b39824d37553bd68c396ab43
vrothx00 <vrothx00@gmail.com>
2023-04-12 15:55:59 +0200

    Refactor the project

--- addOne.java
+++
@@ -1,9 +0,0 @@
-/*
- *  Declare and implement a function called addOne.
- * Your function should accept a single int argument and
- * return the result of adding one to its argument.
- */
-
- int addOne(int num) {
-	return num + 1;
- }
