91dfb154381b182892a9468508282adfccd8e2d5
vrothx00 <vrothx00@gmail.com>
2023-05-19 18:16:38 +0200

    Extend solution khansole project

--- week-4/khansole_academy.py
+++ week-4/khansole_academy.py
@@ -1,17 +1,27 @@
 import random
 
-i = 0
+MIN_RANDOM = 10
+MAX_RANDOM = 99
+COUNTER = 3
 
 def main():
     print("Khansole Academy")
     # TODO: your code here
-    sum = generate_nums()
-    answer = ask_questions()
-    check_answer(answer, sum)
+    row = 0
+    while row < COUNTER:
+        sum = generate_nums()
+        answer = ask_questions()
+        counter_row = sum == answer
+        if counter_row:
+            row += 1
+        else:
+            row = 0
+        check_answer(answer, sum, row)
+    print("Congratulations! You mastered addition.")
 
 def generate_nums():
-    num1 = random.randint(10, 99)
-    num2 = random.randint(10, 99)
+    num1 = random.randint(MIN_RANDOM, MAX_RANDOM)
+    num2 = random.randint(MIN_RANDOM, MAX_RANDOM)
     print(f"What is {num1} + {num2}?")
     sum = num1 + num2
     return sum
@@ -20,12 +30,13 @@ def ask_questions():
     answer = int(input("Your answer: "))
     return answer
 
-def check_answer(answer, sum):
+def check_answer(answer, sum, row):
     if answer != sum:
         print("Incorrect.")
         print(f"The expected answer is {sum}")
     else:
         print("Correct!")
+        print(f"You've gotten {row} correct in a row.")
 
 
 if __name__ == '__main__':
