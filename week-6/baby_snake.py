c1716a4b3307a34f186a0516e456c17511f72d5f
vrothx00 <vrothx00@gmail.com>
2023-05-29 19:39:31 +0200

    Init baby snake

---
+++ week-6/baby_snake.py
@@ -0,0 +1,19 @@
+from graphics import Canvas
+import time
+import random
+    
+CANVAS_WIDTH = 400
+CANVAS_HEIGHT = 400
+SIZE = 20
+
+# if you make this larger, the game will go slower
+DELAY = 0.1 
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    
+    # TODO: your code here
+        
+
+if __name__ == '__main__':
+    main()
