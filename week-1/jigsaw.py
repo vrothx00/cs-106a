ada1c7215421871b6a5de5e2432e8dbeb15b1494
vrothx00 <vrothx00@gmail.com>
2023-06-05 14:15:08 +0200

    Update assign week 1

--- week-1/jigsaw.py
+++ week-1/jigsaw.py
@@ -1,45 +1,45 @@
-from karel.stanfordkarel import *
+# from karel.stanfordkarel import *
 
-"""
-File: PuzzleKarel.py
---------------------
-Karel should finish the puzzle by picking up the last beeper
-(puzzle piece) and placing it in the right spot. Karel should
-end in the same position Karel starts in -- the bottom left
-corner of the world.
-"""
+# """
+# File: PuzzleKarel.py
+# --------------------
+# Karel should finish the puzzle by picking up the last beeper
+# (puzzle piece) and placing it in the right spot. Karel should
+# end in the same position Karel starts in -- the bottom left
+# corner of the world.
+# """
 
 
-def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
-    walk()
-    pick_beeper()
-    move()
-    turn_left()
-    walk()
-    put_beeper()
-    turn_right()
+# def main():
+#     """
+#     You should write your code to make Karel do its task in
+#     this function. Make sure to delete the 'pass' line before
+#     starting to write your own code. You should also delete this
+#     comment and replace it with a better, more descriptive one.
+#     """
+#     walk()
+#     pick_beeper()
+#     move()
+#     turn_left()
+#     walk()
+#     put_beeper()
+#     turn_right()
 
-    move()
-    move()
-    turn_right()
-    turn_left()
-    walk()
-    move()
-    turn_right()
+#     move()
+#     move()
+#     turn_right()
+#     turn_left()
+#     walk()
+#     move()
+#     turn_right()
 
-def turn_right():
-    turn_left()
-    turn_left()
+# def turn_right():
+#     turn_left()
+#     turn_left()
 
-def walk():
-    move()
-    move()
+# def walk():
+#     move()
+#     move()
 from karel.stanfordkarel import *
 
 """
